1d2ed2c5dd1668079d113c4ff43cd6d3408cac31
Chaterine <dhitata052004@gmail.com>
2023-12-08 09:37:36 +0700

    commit 1

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php include 'header.php';?>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img class="rounded-circle" width="300" height="300" src="https://wallpapercave.com/wp/yoGXZkg.jpg" alt="">
+          <h1 class="display-4">AVENGERS</h1>
+          <p class="lead">Marvel Cinematic Universe</p>
+        </div>
+        </div>
+
+        <div class="container text-center" id="about">
+            <div class="row">
+                <div class="col">
+                    <h1>ABOUT</h1>
+                </div>
+            </div>
+            </div>
+
+<div class="row">
+    <div class="col text-justify">
+      Avengers bekerja sama untuk melindungi dunia dari ancaman luar biasa, terutama musuh-musuh yang sulit diatasi oleh satu pahlawan super saja. 
+    </div>
+    <div class="col">
+     Avengers sering berusaha mencerminkan keberagaman dengan menyertakan karakter dari berbagai latar belakang, baik dalam hal kebangsaan, gender, atau ras.
+    </div>
+
+</div>
+
+        </div>
+      </div>
+      
+      </div>
+      </div>
+
+      <div class="card-deck mt-5 ml-5 mr-5">
+        <div class="card">
+          <img class="card-img-top"  src="http://fc09.deviantart.net/fs70/f/2012/134/3/e/the_avengers___ironman_by_stephencanlas-d4zpaxl.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">IRON MAN</h5>
+            <p class="card-text">Pendiri tim superhero Avengers</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top"  src="https://img.ecartelera.com/noticias/27200/27246-m.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">CAPTAIN AMERIKA</h5>
+            <p class="card-text">Pahlawan super yang mereprentasikan negara amerika</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://i.pinimg.com/originals/0f/d8/b8/0fd8b8568460691a7e1afac0e6d172bd.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">THOR</h5>
+            <p class="card-text">Dikenal sebagai dewa petir</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <?php include 'form.php';?>
+
+      <?php include 'footer.php';?>
